3232a2c6f94589741490176351241ea75ed64129
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-09-20 11:48:04 -0300

    feat: adding ImageButton clicker

--- app/src/main/java/com/example/cinema/PreferencesActivity.kt
+++ app/src/main/java/com/example/cinema/PreferencesActivity.kt
@@ -1,29 +1,20 @@
 package com.example.cinema
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_preferences)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val menuImageButton = findViewById<ImageView>(R.id.imageButton)
-
-        menuImageButton.setOnClickListener{
-            val intent = Intent(this, PreferencesActivity::class.java)
-            startActivity(intent)
         }
     }
 }
